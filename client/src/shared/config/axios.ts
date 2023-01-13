c0f6ae2abb4d0a99eb71d09dcb8dff9cea2cf448
mardanniel <mginturo@you-source.com>
2023-01-13 16:10:34 +0800

    update config

--- client/src/shared/config/axios.ts
+++ client/src/shared/config/axios.ts
@@ -1,11 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const cancelTokenSource = axios.CancelToken.source();
 
 export const axiosInstance = axios.create({
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   },
-  cancelToken: cancelTokenSource.token
-})
+  cancelToken: cancelTokenSource.token,
+});
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
